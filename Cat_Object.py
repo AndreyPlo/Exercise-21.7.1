95abf3b2c14bb184c283f149a7050e612c544348
AndreyPlo <andrey.plokhotnichenko@gmail.com>
2023-10-13 14:09:59 +0300

    add task

--- Cat_Object.py
+++ Cat_Object.py
@@ -1,13 +1,13 @@
 from Cat_Class import Cat
 
-кот1 = Cat("Барон", "Мальчик", 2)
+cat1 = Cat("Барон", "Мальчик", 2)
 
-print("Имя 1 кота:", кот1.name)
-print("Пол 1 кота:", кот1.gender)
-print("Возраст 1 кота:", кот1.age)
+print("Имя 1 кота:", cat1.getName())
+print("Пол 1 кота:", cat1.getGender())
+print("Возраст 1 кота:",  cat1.getAge())
 
-кот2 = Cat("Сэм", "Мальчик", 2)
+cat2 = Cat("Сэм", "Мальчик", 2)
 
-print("Имя 2 кота:", кот2.name)
-print("Пол 2 кота:", кот2.gender)
-print("Возраст 2 кота:", кот2.age)
+print("Имя 2 кота:", cat2.getName())
+print("Пол 2 кота:", cat2.getGender())
+print("Возраст 2 кота:", cat2.getAge())
